933dead0f51724da3739766454bd6a8f0e38b386
carljonsson <qWe12345>
2018-07-01 20:41:07 +0200

    Lets commit

---
+++ csvToPlist.py
@@ -0,0 +1,28 @@
+import os
+import plistlib
+import csv
+infile = 'malename.csv'
+outfile = 'ap.plist'
+i = 0
+alist = []
+blist = []
+csvr = csv.reader(open(infile, 'rb'), delimiter=';')
+for row in csvr:
+    if(i>0):
+        for col in row:
+            alist.append(col)
+        pl = dict(
+                apid = alist[0],
+                aptype = alist[1],
+                apname = alist[2],
+                aplat = alist[3],
+                aplon = alist[4],
+                apelev = alist[5],
+                apreg = alist[6],
+                apmuni = alist[7]
+                )
+
+        blist.append(pl)
+        alist = []
+    i+=1
+plistlib.writePlist(blist, outfile)
